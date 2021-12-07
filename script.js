3a06f4a0b38d3b62a4cab04ccd126485d094445d
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-07 16:52:38 +0100

    nieuwe database

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 
 const client = new MongoClient(process.env.FINAL_URL);
-const dbName = "Sterrenkijken";
+const dbName = "sterrenkijker";
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -24,7 +24,7 @@ app.get('/inputPlace', async (req, res) => {
         await client.connect();
         
         const db = client.db(dbName)
-        const colli = db.collection('Sterreninfo');
+        const colli = db.collection('sterrenkijker');
         const findStars = await colli.find({}).toArray();
 
         res.status(200).send(findStars);

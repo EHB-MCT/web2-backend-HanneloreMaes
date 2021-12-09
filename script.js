616d2f44ba2fdf769559cf1ca2e635fc25a249a9
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-09 21:39:51 +0100

    Probleem authentificatie -> Niet Opgelost

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const {MongoClient} = require ('mongodb');
 require('dotenv').config();
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 
 const client = new MongoClient(process.env.FINAL_URL);
 const dbName = "sterrenkijker";
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended = true}))
 
 app.use(express.static("public"));
 
